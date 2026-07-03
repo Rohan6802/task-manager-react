d42da1ac892a9aa126e3e1e80692cd9cc2edcb07
Rohan Sharma <rohansharma682002@gmail.com>
2026-07-03 17:29:14 +0530

    Explicitly allow preflight OPTIONS requests and headers in CORS configuration

--- server/index.js
+++ server/index.js
@@ -12,8 +12,6 @@ const PORT = process.env.PORT || 5000;
 
 connetDB();
 
-const cors = require("cors");
-
 const allowedOrigins = [
   "http://localhost:3000",
   "https://task-manager-react-three-kappa.vercel.app/",
